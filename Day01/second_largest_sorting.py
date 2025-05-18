fd154961e02417633178fe9f8a58df85073ac532
CHETAN SAI CHENNURU <135511031+chetan3129@users.noreply.github.com>
2025-05-18 12:54:42 +0530

    Update second_largest_sorting.py

--- Day01/second_largest_sorting.py
+++ Day01/second_largest_sorting.py
@@ -1,1 +1,11 @@
+# Approach 1: Sorting (Naive)
+def getSecondLargest(arr):
+  n = len(arr)
+  arr.sort() # Sort the array in non-decreasing order
+  for i in range(n - 2, -1, -1):
+    if arr[i] != arr[n - 1]:
+      return arr[i]
+  return -1
 
+# Example:
+print(getSecondLargest([12, 35, 1, 10, 34, 1]))  # 34
